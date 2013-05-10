d3c2e329c22a7f91450860d8426ee1a3c6481fca
luckfox <luckfox0927@gmail.com>
2013-05-10 18:25:50 +0800

    add reference.*

---
+++ forGitDemo/reference.h
@@ -0,0 +1,14 @@
+//
+//  refreence.h
+//  forGitDemo
+//
+//  Created by Administrator on 13/5/10.
+//  Copyright (c) 2013年 Administrator. All rights reserved.
+//
+
+#ifndef __forGitDemo__reference__
+#define __forGitDemo__reference__
+
+#include <iostream>
+
+#endif /* defined(__forGitDemo__reference__) */
---
+++ forGitDemo/reference.mm
@@ -0,0 +1,9 @@
+//
+//  refreence.mm
+//  forGitDemo
+//
+//  Created by Administrator on 13/5/10.
+//  Copyright (c) 2013年 Administrator. All rights reserved.
+//
+
+#include "reference.h"
